fab4501e270d3afd405420cf8cdaac091a70b48e
Shun <nsfgdjpgbdh20@gmail.com>
2025-05-04 00:52:33 +0900

    依存関係の修正とwhatwg-fetch追加、npm install --legacy-peer-deps実施

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,51 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { Platform } from "react-native";
-import { ThemeProvider } from "@/context/theme-context";
-import { ErrorBoundary } from "./error-boundary";
-
-export const unstable_settings = {
-  initialRouteName: "(tabs)",
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    ...FontAwesome.font,
-  });
-
-  useEffect(() => {
-    if (error) {
-      console.error(error);
-      throw error;
-    }
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ErrorBoundary>
-      <ThemeProvider>
-        <RootLayoutNav />
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+import FontAwesome from "@expo/vector-icons/FontAwesome";  
+import { useFonts } from "expo-font";  
+import { Stack } from "expo-router";  
+import * as SplashScreen from "expo-splash-screen";  
+import { useEffect } from "react";  
+import { Platform } from "react-native";  
+import { ThemeProvider } from "@/context/theme-context";  
+import { ErrorBoundary } from "./error-boundary";  
+import { NavigationContainer } from '@react-navigation/native';  
+  
+export const unstable_settings = {  
+  initialRouteName: "(tabs)",  
+};  
+  
+// Prevent the splash screen from auto-hiding before asset loading is complete.  
+SplashScreen.preventAutoHideAsync();  
+  
+export default function RootLayout() {  
+  const [loaded, error] = useFonts({  
+    ...FontAwesome.font,  
+  });  
+  
+  useEffect(() => {  
+    if (error) {  
+      console.error(error);  
+      throw error;  
+    }  
+  }, [error]);  
+  
+  useEffect(() => {  
+    if (loaded) {  
+      SplashScreen.hideAsync();  
+    }  
+  }, [loaded]);  
+  
+  if (!loaded) {  
+    return null;  
+  }  
+  
+  return (  
+    <NavigationContainer>  
+      <ErrorBoundary>  
+        <ThemeProvider>  
+          <RootLayoutNav />  
+        </ThemeProvider>  
+      </ErrorBoundary>  
+    </NavigationContainer>  
+  );  
 }
 
 function RootLayoutNav() {
