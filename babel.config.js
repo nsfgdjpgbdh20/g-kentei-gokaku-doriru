ad0ab6e5cded441422ffdc9770f96999cf645f82
Shun <nsfgdjpgbdh20@gmail.com>
2025-05-03 13:25:22 +0900

    Configure babel-preset-expo for import.meta and remove deprecated plugin

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['babel-preset-expo', { unstable_transformImportMeta: true }]],
     plugins: [
-      // Required for expo-router
-      'expo-router/babel',
+      // Required for expo-router - Removed as it's included in babel-preset-expo now
+      // 'expo-router/babel',
     ],
   };
 }; 
